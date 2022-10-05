5c9f08281eb19c525d8301e1dd6b8f72feb6ef17
erol1098 <erol1098@gmail.com>
2022-10-05 22:53:26 +0300

    Logo Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
 import { modeActions } from '../../redux/mode-slice';
 import Button from '../Button/Button';
 import styles from './Header.module.css';
+
+import Logo from '../../assets/img/last.fm-48.png';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -15,7 +19,9 @@ const Header = () => {
 
   return (
     <header className={`${styles.header} ${styles[mode]}`}>
-      <div onClick={() => navigate('/')}>logo</div>
+      <div onClick={() => navigate('/')}>
+        <img src={Logo} alt='last.fm logo' />
+      </div>
       <h1 onClick={() => navigate('/')}>last.fm</h1>
       <Button variant='secondary' onClick={handleChangeTheme}>
         {mode === 'light' ? 'Dark' : 'Light'}
